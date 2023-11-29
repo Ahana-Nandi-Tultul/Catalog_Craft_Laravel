1c96d8907753520dbeef9172ba4194d067ac81f4
Ahana Nandi <ahananondi6@gmail.com>
2023-11-29 16:58:56 +0600

    add front page and correct the design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,12 @@
                 {{ $slot }}
             </main>
         </div>
+        <footer class="bg-dark text-light p-4">
+                 <div class="container">
+                <h5 class="text-center">&copy; 2023 Catalog Craft || All rights reserved.</h5>
+            
+                </div>
+            </footer>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,7 +14,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
+    <body class="font-sans text-gray-900 antialiased" style = "background-color: #00264d;">
         <div>
             <nav class="navbar navbar-expand-lg bg-light">
             <div class="container-fluid">
@@ -49,6 +49,12 @@
                 {{ $slot }}
             </div>
         </div>
+        <footer class="bg-dark text-light mt-5 p-4">
+                     <div class="container">
+                    <h5 class="text-center">&copy; 2023 Catalog Craft || All rights reserved.</h5>
+                
+                    </div>
+                </footer>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
